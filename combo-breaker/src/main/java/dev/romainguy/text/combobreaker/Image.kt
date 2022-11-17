b71362cec6fec5c9ae722d2617fea91b8e87e9b6
Romain Guy <romainguy@curious-creature.com>
2022-11-16 20:39:54 -0800

    Improve the bitmap contouring algorithm
    The new algorithm can handle multiple contours and images without a
    transparent border.

--- combo-breaker/src/main/java/dev/romainguy/text/combobreaker/Image.kt
+++ combo-breaker/src/main/java/dev/romainguy/text/combobreaker/Image.kt
@@ -17,91 +17,102 @@
 package dev.romainguy.text.combobreaker
 
 import android.graphics.Bitmap
-import android.graphics.Paint
 import android.graphics.Path
 
-internal val HorizontalShift = intArrayOf(
-     0,  1,  0,  1,
-     0,  0,  0,  0,
-    -1,  0,  0,  1,
-    -1, -1,  0,  0
-)
-
-internal val VerticalShift = intArrayOf(
-     0,  0,  1,  0,
-    -1, -1,  0, -1,
-     0,  0,  1,  0,
-     0,  0,  1,  0
-)
-
-// TODO: Rewrite marching squares algorithm to handle multiple contours
-//       and to be more robust (need to handle diagonal cases)
-internal fun direction(pixels: IntArray, index: Int, stride: Int, threshold: Int): Int {
-    // Set bit to 1 when alpha channel is >= threshold, 0 otherwise
-    val key =
-        ((((pixels[index]              ushr 24) - threshold) ushr 31) xor 1 shl 3) or
-        ((((pixels[index + 1]          ushr 24) - threshold) ushr 31) xor 1 shl 2) or
-        ((((pixels[index + stride]     ushr 24) - threshold) ushr 31) xor 1 shl 1) or
-        ((((pixels[index + stride + 1] ushr 24) - threshold) ushr 31) xor 1)
-
-    return index + HorizontalShift[key] + VerticalShift[key] * stride
-}
-
+/**
+ * Extract the contours of this [Bitmap] as a [Path]. The contours are traced by following opaque
+ * pixels, as defined by [alphaThreshold]. Any pixel with an alpha channel value greater than the
+ * specified [alphaThreshold] is considered opaque.
+ *
+ * After the contours are built, a simplification pass is performed to reduce the complexity of the
+ * paths. The [minAngle] parameter defines the minimum angle between two angles in the contour
+ * before they are collapsed. For instance, passing a [minAngle] of 45 means that the final path
+ * will not contain adjacent segments with an angle greater than 45 degrees.
+ */
 fun Bitmap.toContour(
-    alphaThreshold: Float = 0.0f
+    alphaThreshold: Float = 0.0f,
+    minAngle: Float = 15.0f
 ): Path {
     val w = width
     val h = height
-    // TODO: Use guard band
+
     val pixels = IntArray(w * h)
     getPixels(pixels, 0, w, 0, 0, w, h)
 
-    // Increase threshold by 1 for direction() to work as expected
-    val threshold = (alphaThreshold * 255.0f + 1.0f).toInt().coerceIn(0, 255)
+    // Increase threshold by 1 to make the branchless function sample() work
+    val threshold = (alphaThreshold * 255.0f + 1).toInt().coerceIn(0, 255)
 
-    // TOD: We can get rid of this step once we move to a better implementation of the
-    //      marching sqaures algorithm
-    var start = -1
-    for (i in pixels.indices) {
-        // Use >= comparison as we bumped threshold by 1
-        if (pixels[i] ushr 24 >= threshold) {
-            start = i - w - 1 // TODO: not safe until we have a guard band
-            break
+    val contours = ContourSet()
+
+    for (y in 0 until (h - 1)) {
+        val y0 = y.toFloat()
+        val y5 = y + 0.5f
+        val y1 = y + 1.0f
+
+        val rowIndex = y * w
+
+        for (x in 0 until (w - 1)) {
+            val x0 = x.toFloat()
+            val x5 = x + 0.5f
+            val x1 = x + 1.0f
+
+            val index = rowIndex + x
+            val a = pixels.sample(index, threshold)
+            val b = pixels.sample(index + 1, threshold)
+            val c = pixels.sample(index + w, threshold)
+            val d = pixels.sample(index + 1 + w, threshold)
+
+            when (quadKey(a, b, c, d)) {
+                0x1 -> contours.addLine(x0, y5, x5, y0)
+                0x2 -> contours.addLine(x5, y0, x1, y5)
+                0x3 -> contours.addLine(x0, y5, x1, y5)
+                0x4 -> contours.addLine(x5, y1, x0, y5)
+                0x5 -> contours.addLine(x5, y1, x5, y0)
+                0x6 -> {
+                    contours.addLine(x5, y0, x1, y5)
+                    contours.addLine(x0, y1, x0, y5)
+                }
+                0x7 -> contours.addLine(x5, y1, x1, y5)
+                0x8 -> contours.addLine(x1, y5, x5, y1)
+                0x9 -> {
+                    contours.addLine(x0, y5, x5, y0)
+                    contours.addLine(x1, y5, x5, y1)
+                }
+                0xA -> contours.addLine(x5, y0, x5, y1)
+                0xB -> contours.addLine(x0, y5, x5, y1)
+                0xC -> contours.addLine(x1, y5, x0, y5)
+                0xD -> contours.addLine(x1, y5, x5, y0)
+                0xE -> contours.addLine(x5, y0, x0, y5)
+                // else, full transparent or full opaque quads, 0x0 and 0xF
+            }
         }
     }
 
-    var i = 0
-    var index = direction(pixels, start, w, threshold)
-
-    val sx = index % w - 1
-    val sy = index / w - 1
-
+    // TODO: Return a list of paths
     val path = Path()
+    val size = contours.size
+    for (i in 0 until size) {
+        val contour = if (minAngle < 1.0f)
+            contours[i]
+        else
+            contours[i].simplify(minAngle)
 
-    var px = sx.toFloat()
-    var py = sy.toFloat()
-    var pm = Float.NaN
+        val points = contour.points[0]
+        path.moveTo(points, contour.points[1])
 
-    path.moveTo(px, py)
-
-    while (i++ < pixels.size) {
-        index = direction(pixels, index, w, threshold)
-
-        val x = index % w - 1
-        val y = index / w - 1
-
-        val m = (y - py) / (x - px)
-        px = x.toFloat()
-        py = y.toFloat()
-
-        if (m != pm) {
-            path.lineTo(px, py)
+        for (j in 1 until contour.count) {
+            val index = j * 2
+            path.lineTo(contour.points[index], contour.points[index + 1])
         }
-
-        pm = m
-
-        if (index == start) break
     }
 
     return path
 }
+
+@Suppress("NOTHING_TO_INLINE")
+private inline fun IntArray.sample(index: Int, threshold: Int) =
+    (((this[index] ushr 24) - threshold) ushr 31) xor 1
+
+@Suppress("NOTHING_TO_INLINE")
+private inline fun quadKey(a: Int, b: Int, c: Int, d: Int) =
+    a or (b shl 1) or (c shl 2) or (d shl 3)
